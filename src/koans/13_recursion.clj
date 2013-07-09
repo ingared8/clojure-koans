7819a2e1e54ae8fb58e707cc7c64425ad694e3c9
Alejandro Gómez <alejandro@dialelo.com>
2013-07-09 20:31:25 +0200

    13 - recursion

--- src/koans/13_recursion.clj
+++ src/koans/13_recursion.clj
@@ -1,20 +1,28 @@
 (defn is-even? [n]
   (if (= n 0)
-    __
-    (___ (is-even? (dec n)))))
+   true
+    (not (is-even? (dec n)))))
 
 (defn is-even-bigint? [n]
   (loop [n   n
          acc true]
     (if (= n 0)
-      __
+      acc
       (recur (dec n) (not acc)))))
 
 (defn recursive-reverse [coll]
-  __)
+  (loop [xs  coll
+         acc []]
+    (if (empty? xs)
+      acc
+      (recur (rest xs) (cons (first xs) acc)))))
 
 (defn factorial [n]
-  __)
+  (loop [n   n
+         acc 1]
+    (if (= n 2)
+      acc
+      (recur (dec n) (* n acc)))))
 
 (meditations
   "Recursion ends with a base case"
